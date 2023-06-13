cd5546679fec45b2b9c573ffb14c611e0e20672f
Kevin <55450760+kevinbopp@users.noreply.github.com>
2023-06-13 03:21:37 -0400

    Moved old code files to old_code folder

---
+++ old_code/SearchContact_v2.php
@@ -0,0 +1,60 @@
+<?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    $inData = getRequestInfo();
+    $searchTerm = $inData["searchTerm"]; // Change "searchTerm" to the actual key name you expect in the request
+
+    $conn = new mysqli("localhost", "root", "group16COP4331", "COP4331");
+    if ($conn->connect_error) 
+    {
+        returnWithError( $conn->connect_error );
+    } 
+    else
+    {
+        $stmt = $conn->prepare("SELECT ID, userID, FirstName, LastName, Email, Phone FROM Contacts WHERE UserID=? AND (FirstName LIKE ? OR LastName LIKE ? OR Email LIKE ? OR Phone LIKE ?)");
+        
+        $search = "%$searchTerm%"; // Add wildcard characters to the search term
+        
+        $stmt->bind_param("issss", $inData["userID"], $search, $search, $search, $search);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        $contacts = [];
+        while ($row = $result->fetch_assoc())
+        {
+            $contacts[] = $row;
+        }
+
+        $stmt->close();
+        $conn->close();
+
+        returnWithInfo( json_encode($contacts) );
+    }
+    
+    // Linebreak request stuff
+	
+	function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError( $err )
+    {
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+
+    function returnWithInfo($contacts) {
+        sendResultInfoAsJson($contacts);
+    }
+
+?>
